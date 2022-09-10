8547c21379ed209a3c67562cc87d4c07346e0793
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2022-09-10 15:17:20 -0300

    Create code_lab1.r

---
+++ code_lab1.r
@@ -0,0 +1,98 @@
+library(tidyverse)
+#install.packages("tidyverse")
+
+dados <- read.table("LAB1Q1.txt",header = T)
+
+summary(dados)
+
+
+dados %>% group_by(Sexo) %>% summarise(mean(Tempo,na.rm=T))
+dados %>% group_by(Treinamento) %>% summarise(mean(Tempo,na.rm=T))
+dados %>% group_by(Treinamento,Sexo) %>% summarise(mean(Tempo,na.rm=T))
+
+boxplot(dados$Tempo~dados$Sexo+dados$Treinamento)
+
+t.test(dados$Tempo~dados$Treinamento)
+
+df_spread <- spread(dados,Treinamento,Tempo)
+
+df_spread$dif_tempo <- df_spread$Depois-df_spread$Antes
+
+#dados2 = reshape(dados, 
+ #                timevar = "Tempo",
+  #               v.names = "Placa",
+   #              idvar = c("Id", "Sexo"),
+    #             direction = "wide")
+
+m1 <- lm(df_spread$dif_tempo~1)
+summary(m1)
+
+7.1903-0.9169
+
+m1 <- lm(df_spread$dif_tempo~df_spread$Sexo)
+summary(m1)
+
+
+m1 <- lm(Depois~Sexo+Antes,data = df_spread)
+summary(m1)
+
+
+m2 <- lm(dif_tempo~Sexo+Antes,data = df_spread)
+summary(m2)
+
+
+predict(m1,data.frame(Sexo="M",Antes=mean(df_spread$Antes, na.rm=TRUE)),se=F)
+
+predict(m1,data.frame(Sexo="F",Antes=mean(df_spread$Antes, na.rm=TRUE)),se=F)
+
+predict(m2,data.frame(Sexo="M",Antes=mean(df_spread$Antes, na.rm=TRUE)),se=F)
+
+predict(m2,data.frame(Sexo="F",Antes=mean(df_spread$Antes, na.rm=TRUE)),se=F)
+
+
+#####################33
+
+
+dados <- read.table("insulin.dat",header = F)
+colnames(dados) <- c("id","tempo","nas","grupo")
+
+#dados$tempo <- as.factor(dados$tempo)
+
+dados %>% group_by(tempo) %>% summarise(media=mean(nas),desvio=sd(nas))
+
+#install.packages("corrplot")
+library(corrplot)
+
+df_spread <- spread(dados,tempo,nas)
+df_spread<- df_spread[,-c(1,2)]
+
+dados_cor <- cor(df_spread)
+corrplot(dados_cor)
+
+plot(nas~as.numeric(tempo),data = dados, pch = ".", xlab = "Tempo(Horas)", ylab="Nivel de a ̧c ́ucar
+      no sangue")
+
+library(lattice)
+
+xyplot(nas~as.numeric(tempo), data = dados, type = 'l', group = id, xlab = 'Tempo(horas)', col.line
+  = 'gray20', ylab = 'Nivel de açucar no sangue')
+
+
+
+s = sample(unique (dados$id), size = 5)
+
+plot(nas~as.numeric(tempo), data = dados, col = "gray50", xlab = "Tempo", ylab = "N ́ıvel de a ̧cuucar
+      no sangue")
+
+for (i in 1:5) {
+  lines (dados$tempo[dados$id == s[i]],
+         dados$nas[dados$id == s[i]], lwd = 1.5)
+}
+
+library(gplots)
+#install.packages("gplots")
+plotmeans(nas~tempo, data=dados,barwidth = 3)
+
+## c
+
+
