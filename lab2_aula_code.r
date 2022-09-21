cbb619e7d5dd30cc3b189810c01bfea69342e245
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2022-09-21 08:40:06 -0300

    Update lab2_aula_code.r

--- lab2_aula_code.r
+++ lab2_aula_code.r
@@ -24,12 +24,35 @@ xyplot(y~as.numeric(day), data = dados, type = 'l', group = id, xlab = 'Dia', co
 
 y_p_p=mean(dados$y)
 SQT=sum((dados$y-y_p_p)^2)
+n=nrow(dados)
 
-#soma dos quadrados dos grupos
+#soma dos quadrados entre grupos
 
 ids_media <- dados %>% group_by(id) %>% summarise(mean(y))
 SQE <- sum((ids_media$`mean(y)`-y_p_p)^2)
 
+#SOMA DOS QUADRADOS Dentro
+
 SQD <- SQT-SQE
+g=nrow(ids_media)
 
+#QUADRADO MEDIO ENTRE
 
+QME=SQE/(g-1)
+
+#QUADRADO MEDIO DENTRO
+
+QMD=SQD/(n-g)
+
+#estatistica F
+#F 53,216
+
+F_SNED=QME/QMD
+F_SNED
+qf(0.05,df1 =53 ,df2 = 216)
+
+#n rejeita h0
+
+dados.aov = aov(y~day + Error(id), data = dados)
+
+summary(dados.aov)
