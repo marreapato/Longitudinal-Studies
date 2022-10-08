d0cff1e244683b48b3ff0daaa99f6c9ab69592ce
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2022-10-08 20:16:49 -0300

    Update lab3.r

--- lab3.r
+++ lab3.r
@@ -11,9 +11,32 @@ idata = data.frame(tempo=ordered(1:5))
 library(car)
 ajuste=Anova(arv.mod, idata=idata, idesign=~tempo, type='III')
 
-summary(Anova(arv.mod, idata=idata, idesign=~tempo, type="III"))
+ajuste
+
+ajuste2=Anova(arv.mod, idata=idata, idesign=~tempo, type='III',test.statistic = "Wilks")
+
+ajuste2
+
+ajuste3=Anova(arv.mod, idata=idata, idesign=~tempo, type='III',test.statistic = "Roy")
+
+ajuste3
+
+ajuste4=Anova(arv.mod, idata=idata, idesign=~tempo, type='III',test.statistic = "Hotelling-Lawley")
+
+ajuste4
+
+sumario_manova=summary(Anova(arv.mod, idata=idata, idesign=~tempo, type="III"))
+
+sumario_manova$multivariate.tests$tempo
+
+sumario_manova$sphericity.tests
+
+sumario_manova
 
 ajuste$P
+
+
+
 
 
 ####################################################
@@ -25,12 +48,27 @@ dados2 <- read.csv("fosfato.csv")
 dados <- dados2
 
 
-arv.mod = lm(cbind(dados$X0, dados$X0.5, dados$X1, dados$X1.5, dados$X2, dados$X3, dados$X4, dados$X5) ~ 1, data=dados)
+arv.mod = lm(cbind(dados$X0, dados$X0.5, dados$X1, dados$X1.5, dados$X2, dados$X3, dados$X4, dados$X5) ~ Grupo, data=dados)
 
 idata = data.frame(tempo=ordered(1:8))
 
 library(car)
 ajuste=Anova(arv.mod, idata=idata, idesign=~tempo, type='III')
+
+ajuste
+
+ajuste2=Anova(arv.mod, idata=idata, idesign=~tempo, type='III',test.statistic = "Wilks")
+
+ajuste2
+
+ajuste3=Anova(arv.mod, idata=idata, idesign=~tempo, type='III',test.statistic = "Roy")
+
+ajuste3
+
+ajuste4=Anova(arv.mod, idata=idata, idesign=~tempo, type='III',test.statistic = "Hotelling-Lawley")
+
+ajuste4
+
 
 summary(Anova(arv.mod, idata=idata, idesign=~tempo, type="III"))
 
