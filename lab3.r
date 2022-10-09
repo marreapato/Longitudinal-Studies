4a5ea4aa2f4dee35c5f125650374b28cbca49d93
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2022-10-09 17:19:26 -0300

    Update lab3.r

--- lab3.r
+++ lab3.r
@@ -33,11 +33,49 @@ sumario_manova$sphericity.tests
 
 sumario_manova
 
-ajuste$P
+ajuste$P$tempo
 
 
+  
+dados <- read.table("LAB2Q1.txt")
+
+medias_tempos=dados %>% group_by(day) %>% summarise(media=mean(y),desvios=sd(y))#medias e devios
+medias_tempos
+
+polinomio=ajuste$P$tempo#polinomi
+
+ajuste$SSP$tempo
+
+diag(ajuste$SSP$tempo)
+
+df_spread <- spread(dados,day,y)
+df_spread<- df_spread[,-c(1)]
+m=length(unique(dados1$id)) #individuos
 
 
+dfmat <- t(as.matrix(polinomio))%*%(t(as.matrix(df_spread))%*%as.matrix(df_spread)-m*as.matrix(medias_tempos[,2])%*%
+  t(as.matrix(medias_tempos[,2])))%*%(as.matrix(polinomio))#equivale a
+
+ssp_matrix=ajuste$SSP$tempo#sum of squares
+
+ssr_matrix <- sumario_manova$SSPE$tempo#sum of square residuals
+
+ssp_matrix[upper.tri(ssp_matrix)] <- NA
+ssp_matrix
+
+ssr_matrix[upper.tri(ssr_matrix)] <- NA
+ssr_matrix
+
+ssp_diag=diag(ssp_matrix)
+qmrs=diag(ssr_matrix)/(m-1)
+
+ssp_diag/qmrs
+n_1=length(colnames(df_spread))-1
+
+ssp_matrix
+ssr_matrix
+
+round(pf(ssp_diag/qmrs,1,m-1,lower.tail = F),4)
 
 ####################################################
 
